3f3d93b7886f964a229839797e224a5cc7bbe05a
ecampos14 <88864112+ecampos14@users.noreply.github.com>
2023-05-21 21:08:55 -0300

    Ajuste indentacao do codigo

--- app/controllers/distanceController.js
+++ app/controllers/distanceController.js
@@ -1,83 +1,82 @@
 const places = [
-    {
-      id: 1,
-      name: 'Parque da Cidade',
-      latitude: -23.221112,
-      longitude: -23.221112,
-    },
-    {
-      id: 2,
-      name: 'Praça Ulisses Guimarães',
-      latitude: -23.180038,
-      longitude: -45.884357,
-    },
-    {
-      id: 3,
-      name: 'Lagoa do Taquaral',
-      latitude: -22.874863,
-      longitude: -47.048815,
-    },
-    {
-      id: 4,
-      name: 'Jardim Botânico',
-      latitude: -23.550520,
-      longitude: -46.638340,
-    },
-    {
-      id: 5,
-      name: 'Praia de Copacabana',
-      latitude: -22.971964,
-      longitude: -43.182545,
-    },
-  ];
-  
-  function calculateDistance(point1, point2) {
-    const distance = Math.sqrt(
-      Math.pow(point1.latitude - point2.latitude, 2) +
-      Math.pow(point1.longitude - point2.longitude, 2)
-    );
-  
-    return distance;
+  {
+    id: 1,
+    name: 'Parque da Cidade',
+    latitude: -23.221112,
+    longitude: -23.221112,
+  },
+  {
+    id: 2,
+    name: 'Praça Ulisses Guimarães',
+    latitude: -23.180038,
+    longitude: -45.884357,
+  },
+  {
+    id: 3,
+    name: 'Lagoa do Taquaral',
+    latitude: -22.874863,
+    longitude: -47.048815,
+  },
+  {
+    id: 4,
+    name: 'Jardim Botânico',
+    latitude: -23.550520,
+    longitude: -46.638340,
+  },
+  {
+    id: 5,
+    name: 'Praia de Copacabana',
+    latitude: -22.971964,
+    longitude: -43.182545,
+  },
+];
+
+function calculateDistance(point1, point2) {
+  const distance = Math.sqrt(
+    Math.pow(point1.latitude - point2.latitude, 2) +
+    Math.pow(point1.longitude - point2.longitude, 2)
+  );
+
+  return distance;
+}
+
+function getDistanceBetweenPlaces(req, res) {
+  const { id1, id2 } = req.params;
+
+  const place1 = places.find(place => place.id === Number(id1));
+  const place2 = places.find(place => place.id === Number(id2));
+
+  if (place1 && place2) {
+    const distance = calculateDistance(place1, place2);
+
+    res.json({ distance });
+  } else {
+    res.status(404).json({ message: 'Um ou ambos os lugares não foram encontrados' });
   }
-  
-  function getDistanceBetweenPlaces(req, res) {
-    const { id1, id2 } = req.params;
-  
-    const place1 = places.find(place => place.id === Number(id1));
-    const place2 = places.find(place => place.id === Number(id2));
-  
-    if (place1 && place2) {
-      const distance = calculateDistance(place1, place2);
-  
-      res.json({ distance });
-    } else {
-      res.status(404).json({ message: 'Um ou ambos os lugares não foram encontrados' });
-    }
-  }
-  
-  function searchPlaces(req, res) {
-    const { latitude, longitude, radius } = req.query;
-  
-    const centralPoint = {
-      latitude: parseFloat(latitude),
-      longitude: parseFloat(longitude)
-    };
-  
-    const searchRadius = parseInt(radius);
-  
-    const placesWithinRadius = places.map(place => {
-      const distance = calculateDistance(centralPoint, place);
-      return {
-        ...place,
-        distance
-      };
-    }).filter(place => place.distance <= searchRadius);
-  
-    res.json(placesWithinRadius);
-  }
-  
-  module.exports = {
-    getDistanceBetweenPlaces,
-    searchPlaces
+}
+
+function searchPlaces(req, res) {
+  const { latitude, longitude, radius } = req.query;
+
+  const centralPoint = {
+    latitude: parseFloat(latitude),
+    longitude: parseFloat(longitude)
   };
-  
+
+  const searchRadius = parseInt(radius);
+
+  const placesWithinRadius = places.map(place => {
+    const distance = calculateDistance(centralPoint, place);
+    return {
+      ...place,
+      distance
+    };
+  }).filter(place => place.distance <= searchRadius);
+
+  res.json(placesWithinRadius);
+}
+
+module.exports = {
+  getDistanceBetweenPlaces,
+  searchPlaces
+};
--- app/controllers/placesPartIIController.js
+++ app/controllers/placesPartIIController.js
@@ -1,83 +1,83 @@
 const places = [
-    {
-      id: 1,
-      name: 'Parque da Cidade',
-      latitude: -23.221112,
-      longitude: -45.899678,
-    },
-    {
-      id: 2,
-      name: 'Praça Ulisses Guimarães',
-      latitude: -23.180038,
-      longitude: -45.884357,
-    },
-    {
-      id: 3,
-      name: 'Lagoa do Taquaral',
-      latitude: -22.874863,
-      longitude: -47.048815,
-    },
-    {
-      id: 4,
-      name: 'Jardim Botânico',
-      latitude: -23.550520,
-      longitude: -46.638340,
-    },
-    {
-      id: 5,
-      name: 'Praia de Copacabana',
-      latitude: -22.971964,
-      longitude: -43.182545,
-    },
-  ];
+  {
+    id: 1,
+    name: 'Parque da Cidade',
+    latitude: -23.221112,
+    longitude: -45.899678,
+  },
+  {
+    id: 2,
+    name: 'Praça Ulisses Guimarães',
+    latitude: -23.180038,
+    longitude: -45.884357,
+  },
+  {
+    id: 3,
+    name: 'Lagoa do Taquaral',
+    latitude: -22.874863,
+    longitude: -47.048815,
+  },
+  {
+    id: 4,
+    name: 'Jardim Botânico',
+    latitude: -23.550520,
+    longitude: -46.638340,
+  },
+  {
+    id: 5,
+    name: 'Praia de Copacabana',
+    latitude: -22.971964,
+    longitude: -43.182545,
+  },
+];
 
-  function getAllPlaces(req, res) {
-    res.json(places);
-  }
-  
-  function createPlace(req, res) {
-    const { name, latitude, longitude } = req.body;
-    const newPlace = {
-      id: places.length + 1,
+function getAllPlaces(req, res) {
+  res.json(places);
+}
+
+function createPlace(req, res) {
+  const { name, latitude, longitude } = req.body;
+  const newPlace = {
+    id: places.length + 1,
+    name,
+    latitude,
+    longitude,
+  };
+
+  places.push(newPlace);
+  res.status(201).json(newPlace);
+}
+
+function updatePlace(req, res) {
+  const placeId = Number(req.params.id);
+  const { name, latitude, longitude } = req.body;
+  const placeIndex = places.findIndex(place => place.id === placeId);
+
+  if (placeIndex !== -1) {
+    places[placeIndex] = {
+      id: placeId,
       name,
       latitude,
       longitude,
     };
-  
-    places.push(newPlace);
-    res.status(201).json(newPlace);
-  }
-  
-  function updatePlace(req, res) {
-    const placeId = Number(req.params.id);
-    const { name, latitude, longitude } = req.body;
-    const placeIndex = places.findIndex(place => place.id === placeId);
-  
-    if (placeIndex !== -1) {
-      places[placeIndex] = {
-        id: placeId,
-        name,
-        latitude,
-        longitude,
-      };
-      res.json(places[placeIndex]);
-    } else {
-      res.status(404).json({ message: 'Lugar não encontrado' });
-    }
-  }
-  function deletePlace(req, res) {
-    const placeId = Number(req.params.id);
-    const placeIndex = places.findIndex(place => place.id === placeId);
-  
-    if (placeIndex !== -1) {
-      places.splice(placeIndex, 1);
-      res.json({ message: 'Lugar removido com sucesso!' });
+    res.json(places[placeIndex]);
+  } else {
+    res.status(404).json({ message: 'Lugar não encontrado' });
   }
 }
-  
-  module.exports = {
-    getAllPlaces,
-    createPlace,
-    updatePlace,
-    deletePlace,
-  };
+function deletePlace(req, res) {
+  const placeId = Number(req.params.id);
+  const placeIndex = places.findIndex(place => place.id === placeId);
+
+  if (placeIndex !== -1) {
+    places.splice(placeIndex, 1);
+    res.json({ message: 'Lugar removido com sucesso!' });
+  }
+}
+
+module.exports = {
+  getAllPlaces,
+  createPlace,
+  updatePlace,
+  deletePlace,
+};
